91284527ac23b71996f105334959166be96f88f9
랄라수 <lallasu@lallasus-MacBook-Air.local>
2022-12-07 04:56:24 +0900

    rock scissors paper logic - learn python

---
+++ rock_sissors_paper.py
@@ -0,0 +1,21 @@
+import random
+
+def play():
+  user = input("What's your choice? 'r' for rock, 's' for scissors, 'p' for paper\n")
+  computer = random.choice(['r', 's', 'p'])
+  
+  if user == computer:
+    return 'It\'s a tie'
+  
+  # r > s, s > p, p > r
+  if is_win(user, computer):
+    return 'You won!'
+  
+  return 'You lost!'
+  
+def is_win(player, opponent):
+  #return true if player wins
+  # r > s, s > p, p > r
+  if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+    or (player == 'p' and opponent == 'r'):
+    return True
